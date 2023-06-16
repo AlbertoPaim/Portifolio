e1b3a22ce4cad0eaa7e70ea26ba7e63154788523
Alberto Paim <albertoopaim@gmail.com>
2023-06-16 09:12:21 -0300

    changes

---
+++ js/descriptions.js
@@ -0,0 +1,24 @@
+
+    
+
+
+
+let innerSkill = [
+'<h4>HTML5</h4><br>sintaxe<br>Tags semânticas<br>Manipulação de texto<br>Inclusão de imagens e mídia <br>Acessibilidade <br>Tabelas  <br> Formulários <br> Utilização de links',
+
+'<h4>CSS3</h4><br>Seletores <br>Propriedades de estilo <br>Box model <br> Flexbox e Grid <br>Posicionamento <br>Layout responsivo <br>Estilização de elementos de formulário <br>Pseudoclasses e pseudoelementos  <br>Animações e transições <br>',
+
+'<h4>GIT</h4><br>Inicialização de repositórios<br> Comandos Básicos<br> Branches<br> Reverter alterações',
+
+'<h4>GitHub</h4><br>Criação de repositórios<br> Clonagem de repositórios<br> Envio de commits<br> Criar Páginas Pull Requests',
+    
+'<h4>JavaScript</h4><br>Sintaxe <br> Manipulação do DOM <br> Funções <br> Manipulação de arrays e objetos <br> Assincronia e chamadas de API <br> Manipulação de strings  <br> Tratamento de erros <br> Local Storage',
+
+'<h4>TypeScript</h4>Sintaxe <br>Tipos de dados <br> Declaração de variáveis e funções <br> Interfaces <br> Configuração do TypeScript <br>',
+
+'<h4>TailwindCss</h4>Classes utilitárias <br> Personalização <br> FlexBox<br>Grid<br> Animações em geral <br> Manipulação de cores e Gradientes <br> Darkmode <br>',
+
+'<h4>React.js</h4>Learning in progress <br>'
+
+]
+
