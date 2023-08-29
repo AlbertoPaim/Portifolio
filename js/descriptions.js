5d5330e8e5951d43f606a3f0ad94d5ca81778376
Alberto Paim <119620474+AlbertoPaim@users.noreply.github.com>
2023-08-29 18:46:03 -0300

    add node.js

--- js/descriptions.js
+++ js/descriptions.js
@@ -14,12 +14,8 @@ let innerSkill = [
     
 '<h4>JavaScript</h4><br>Sintaxe <br> Manipulação do DOM <br> Funções <br> Manipulação de arrays e objetos <br> Assincronia e chamadas de API <br> Manipulação de strings  <br> Tratamento de erros <br> Local Storage',
 
-'<h4>TypeScript</h4>Sintaxe <br>Tipos de dados <br> Declaração de variáveis e funções <br> Interfaces <br> Configuração do TypeScript <br>',
-
 '<h4>TailwindCss</h4>Classes utilitárias <br> Personalização <br> FlexBox<br>Grid<br> Animações em geral <br> Manipulação de cores e Gradientes <br> Darkmode <br>',
 
-'<h4>React.js</h4>Learning in progress <br>',
-
-'<h4>Next.js</h4>Learning in progress <br>'
+'<h4>Node.js</h4>NPM (Node Package Manager)<br> Módulos e Require <br> Assincronismo e Callbacks <br> Promessas e Async/Await <br> Express.js <br> Middlewares <br> Rotas e Roteamento <br> Gerenciamento de Erros'
 ]
 
